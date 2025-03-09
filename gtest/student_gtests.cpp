bebde790f60641058515546d5c3f38c2d3339539
Julian Renfro <JCRenfro@uci.edu>
2025-03-08 22:25:59 -0800

    Dijkstras implemented & tests for small and medium texts

--- gtest/student_gtests.cpp
+++ gtest/student_gtests.cpp
@@ -3,6 +3,52 @@
 #include "../src/dijkstras.h"
 #include "../src/ladder.h"
 
-TEST(Replace, Me) {
-  EXPECT_TRUE(false);
+
+void printGraph(ostream& out, const Graph& g)
+{
+  out << "CURRENT GRAPH: ";
+  for (auto i: g)
+    for (auto e: i)
+      out << e << " ";
+  out << std::endl;
+}
+
+void printVec(ostream& out, const vector<int> v)
+{
+  out << "Current Vec: ";
+  for (auto i: v)
+    out << i << " ";
+  out << endl;
+}
+
+class Dijkstras: public testing::Test {};
+
+TEST_F(Dijkstras, SmallTxt) {
+  Graph g;
+  file_to_graph("src/small.txt", g);
+  printGraph(std::cout, g);
+
+  vector<int> previous(g.size());
+
+  vector<int> tree = dijkstra_shortest_path(g, 0, previous);
+  printVec(cout, tree);
+
+  auto path = extract_shortest_path(tree, previous, 2);
+  printVec(cout, path);
+
+}
+
+TEST_F(Dijkstras, MedTxt) {
+  Graph g;
+  file_to_graph("src/medium.txt", g);
+  printGraph(std::cout, g);
+
+  vector<int> previous(g.size());
+
+  vector<int> tree = dijkstra_shortest_path(g, 0, previous);
+  printVec(cout, tree);
+
+  auto path = extract_shortest_path(tree, previous, 2);
+  printVec(cout, path);
+
 }
--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -4,13 +4,70 @@
 
 vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous)
 {
-    
+    /*
+        Uses a MinHeap
+        1. Takes all edges for minheap
+        2. While heap not empty,
+            a. extract minimum weight from miniheap
+            b. set to visited
+            c. get all edges from current node
+            d. Check distance to currdistance + weight < currentMin for that node, if it is, update it by chaning distance & previous
+                Also push to minHeap
+        3. Repeat until minheap empty
+        4. return distances
+    */
+    // Inialize Parts
+    int size = G.size();
+    vector<int> distances(size, INF);
+    vector<bool> visited(size, false);
+    priority_queue<pair<int, int>> minHeap; // Minheap pair (vertex, weight)
+    // Set distance of start position to 0
+    distances[source] = 0;
+    previous[source] = -1;
+    // Push current minimum (source)
+    minHeap.push({source, 0});
+    // Start Iteration
+    while (!minHeap.empty()){
+        // Get current minimum node
+        int currMinNode = minHeap.top().first; minHeap.pop();
+            //std::cout << "IN: " << currMinNode << std::endl;
+        // Check if visited, if visited go to next part
+        if (visited[currMinNode]) continue;
+        // If not visited set to visited
+        visited[currMinNode] = true;
+        // Process Edges of current node
+        for (Edge edge: G[currMinNode]){
+            int disToDestNode = distances[currMinNode] + edge.weight;
+            int currDisToDestNode = distances[edge.dst];
+            if (!visited[edge.dst] && disToDestNode < currDisToDestNode){
+                // Update distance to new minimum
+                distances[edge.dst] = disToDestNode;
+                // Set previous node
+                previous[edge.dst] = currMinNode;
+                // Add to heap
+                    //std::cout << "\tPUSHING NODE: " << edge.dst << std::endl;
+                minHeap.push({edge.dst, disToDestNode});
+            }
+        }
+            //std::cout << "MINHEAP TOP IS: " << minHeap.top().first << std::endl;
+    }
+    return distances;
 }
-vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination)
+vector<int> extract_shortest_path(const vector<int>& distances, const vector<int>& previous, int destination)
 {
-
+    vector<int> path;
+    stack<int> rPath;
+    rPath.push(destination);
+    for (int currPrev=previous[destination]; currPrev != -1; currPrev=previous[currPrev])
+        rPath.push(currPrev);
+    while (!rPath.empty()){
+        int c = rPath.top(); rPath.pop();
+        path.push_back(c);
+    }
+    return path;
 }
 void print_path(const vector<int>& v, int total)
 {
-
+    for (int i=0; i < total; ++i)
+        std::cout << v[i] << " ";
 }
